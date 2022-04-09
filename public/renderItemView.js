372e52ca0d4ccc5bb48480d03fba612f841f0a08
niklasHagner <niklas.hagner@outlook.com>
2022-04-09 20:22:11 +0200

    Show tillfälligt sortiment, begränsad tid etc

--- public/renderItemView.js
+++ public/renderItemView.js
@@ -17,33 +17,52 @@
   }
 */
 function renderItemView(item) {
-  var card = item;
-  card.price = Number(card.price).toFixed(2);
+  item.price = Number(item.price).toFixed(2);
 
-  var isExpensive = Number(card.price) > 40;
-  card.themeColor = isExpensive ? 'red' : 'green';
-
-  var ratingIcon = card.rating_rb_overall && Number(card.rating_rb_overall) < 50 ? '👎' : '⭐';
-  var rating = card.rating_rb_overall ? `<p class="rating">${ratingIcon} ${card.rating_rb_overall} <span>(${card.rating_rb_count} reviews)</span></p>` : '';
-  var producer = card.producer ? `<p>${card.producer}</p>` : '';
-  var country = card.country ? `<p>${card.country}</p>` : '';
-  var alcohol = card.alcohol_vol ? ` , <span>${card.alcohol_vol} <span>%</span></span>` : '';
-  var price = card.price ? `<span>${card.price} <span>kr</span></span>` : '';
+  // var ratebeerLinkEl =  item.sysid ? `<a target="_blank" href="https://www.ratebeer.com/beer/${item.sysid}">Ratebeer</a>` : '';
+  // var ratingIcon = item.rating_rb_overall && Number(item.rating_rb_overall) < 50 ? '👎' : '⭐';
+  // var rating = item.rating_rb_overall ? `<p class="rating">${ratingIcon} ${item.rating_rb_overall} <span>(${item.rating_rb_count} reviews)</span></p>` : '';
+  var producer = item.producer ? `<p>${item.producer}</p>` : '';
+  var country = item.country ? `<p>${item.country}</p>` : '';
+  var alcohol = item.alcohol_vol ? ` , <span>${item.alcohol_vol}<span class="percentage-char">%</span></span>` : '';
+  var price = item.price ? `<span>${item.price} <span class="kr">kr</span></span>` : '';
   var price_and_alcohol = price != '' ? `<p>${price} ${alcohol}</p>` : '';
-  var systembolagetUrl =  card.id ? ` https://www.systembolaget.se/${item.sysid}` : '';
-  var img = card.id ? `<img src="https://systembevakningsagenten.se/assets/images/product/id/${card.id}.png" onerror="this.style.display='none'">` : '';
-  // var ratebeerLinkEl =  card.sysid ? `<a target="_blank" href="https://www.ratebeer.com/beer/${item.sysid}">Ratebeer</a>` : '';
+  var systembolagetUrl =  item.id ? ` https://www.systembolaget.se/${item.sysid}` : '';
+  var img = item.id ? `<img src="https://systembevakningsagenten.se/assets/images/product/id/${item.id}.png" onerror="this.style.display='none'">` : '';
+  
+  /*
+    Systembolaget codes for sales conditions
+      used in systembevakningsagenten objects as propname 'class'
+    ----
+    FSN = Fast sortiment valt av Systembolaget. 
+    FSB = Fast sortiment som kvalat in från beställningssortimentet på grund av efterfrågan.
+    BS = Beställningssortimentet 
+    TSE = Tillfälligt sortiment exklusiv, hette tidigare små partier. 
+    TST = Tillfälligt sortiment tid, säljs bara under en begränsad tid.
+    TSV = Tillfälligt sortiment volym, säljs i begränsad mängd.
+    TSLS = Tillfälligt sortiment lokalt och småskaligt.
+    TSS = Tillfälligt sortiment säsong, exempelvis påsköl
+  */
+  let salesInfo = "";
+  switch(item.class) {
+    case "BS": salesInfo = "beställningssortiment"; break;
+    // case "TSE": salesInfo = "exklusivt sortiment"; break;
+    case "TST": salesInfo = "begränsad mängd"; break;
+    case "TSV": salesInfo = "begränsad tid"; break;
+    case "TSLS": salesInfo = "lokalt och småksaligt"; break;
+    case "TSS": salesInfo = "säsongsprodukt"; break;
+  }
  
   var view = `
     <article class="item">
       <header>
-        <a href="${systembolagetUrl}">${card.name}</a>
+        <a href="${systembolagetUrl}">${item.name}</a>
       </header>
       ${img}
-      ${rating}
       ${producer}
       ${country}
       ${price_and_alcohol}
+      ${salesInfo}
     </article>`;
 
   return view;
